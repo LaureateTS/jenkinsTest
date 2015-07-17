8ccf9e061903a83c4690dc5ccfbd0bcdaeedcd1d
Tristan Geraets <tristang@protonmail.com>
2015-07-17 14:11:11 +0200

    cleaned some auth features out to keep testing simple

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using WebApplication1;
 
-[assembly: OwinStartupAttribute(typeof(WebApplication1.Startup))]
+[assembly: OwinStartup(typeof(Startup))]
 namespace WebApplication1
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
